5cbf4df89ff166634ef87ef233ac37623fba20d0
Rocik764 <63047043+Rocik764@users.noreply.github.com>
2021-11-29 23:20:06 +0100

    503 on Heroku, checking if it's image conversion problem.

--- resources/views/partials/image-conversion.blade.php
+++ resources/views/partials/image-conversion.blade.php
@@ -1,4 +1,5 @@
 <picture>
-    <source type="image/webp" srcset="{{ route('image', "{$path}?fm=webp") }}">
-    <img src="{{ route('image', "{$path}") }}" alt="{{ $alt ?? "" }}">
+    <img src="{{ asset('storage/img/skills/bootstrap.png') }}" alt="{{ $alt }}">
+{{--    <source type="image/webp" srcset="{{ route('image', "{$path}?fm=webp") }}">--}}
+{{--    <img src="{{ route('image', "{$path}") }}" alt="{{ $alt ?? "" }}">--}}
 </picture>
