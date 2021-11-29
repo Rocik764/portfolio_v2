194714cee2c60dd2582e0d81f50258e8bfa40e4e
Rocik764 <63047043+Rocik764@users.noreply.github.com>
2021-11-29 23:33:19 +0100

    503 on Heroku, "nodejs app detected" lol.

--- resources/views/partials/image-conversion.blade.php
+++ resources/views/partials/image-conversion.blade.php
@@ -1,5 +1,4 @@
 <picture>
-    <img src="{{ asset('storage/img/skills/bootstrap.png') }}" alt="{{ $alt }}">
-{{--    <source type="image/webp" srcset="{{ route('image', "{$path}?fm=webp") }}">--}}
-{{--    <img src="{{ route('image', "{$path}") }}" alt="{{ $alt ?? "" }}">--}}
+    <source type="image/webp" srcset="{{ route('image', "{$path}?fm=webp") }}">
+    <img src="{{ route('image', "{$path}") }}" alt="{{ $alt ?? "" }}">
 </picture>
