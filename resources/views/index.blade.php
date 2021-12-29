5c58e5f014dcb8cb59cf683b1dc40c36fb7bd82a
Rocik764 <63047043+Rocik764@users.noreply.github.com>
2021-12-29 15:06:45 +0100

    Removed dupa console log...

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -78,7 +78,6 @@
         $(document).ready(function() {
             $('#frontend-button').click();
             let interval = setInterval(function() {
-                console.log('dupa')
                 $('#backend-button').click();
                 clearInterval(interval);
             }, 500);
